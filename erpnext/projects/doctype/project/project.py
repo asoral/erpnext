0a33792f5479e447319a30fb4ce8bafbf6dd931d
Ajay Jogdand <ajayjogdand374@gmail.com>
2021-09-09 17:36:29 +0530

    changes

--- erpnext/projects/doctype/project/project.py
+++ erpnext/projects/doctype/project/project.py
@@ -78,17 +78,25 @@ class Project(Document):
 							task = self.create_task_from_template(template_task_details,required)
 							if task:
 								project_tasks.append(task)
+						self.dependency_mapping(tmp_task_details, project_tasks)
+				for task in template.tasks:
+					if task not in required:
+						template_task_details = frappe.get_doc("Task", task.task)
 						if template_task_details.parent_task and template_task_details.is_group==1:
 							tmp_task_details.append(template_task_details)
 							task = self.create_task_from_template(template_task_details,required)
 							if task:
 								project_tasks.append(task)
+						self.dependency_mapping(tmp_task_details, project_tasks)
+				for task in template.tasks:
+					if task not in required:
+						template_task_details = frappe.get_doc("Task", task.task)
 						if template_task_details.parent_task and template_task_details.is_group==0:
 							tmp_task_details.append(template_task_details)
 							task = self.create_task_from_template(template_task_details,required)
 							if task:
 								project_tasks.append(task)
-					self.dependency_mapping(tmp_task_details, project_tasks)
+						self.dependency_mapping(tmp_task_details, project_tasks)
 			else:
 				for task in template.tasks:
 					template_task_details = frappe.get_doc("Task", task.task)
