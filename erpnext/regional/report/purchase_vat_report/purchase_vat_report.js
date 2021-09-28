3f960965a3c27e15a6346be53b7446c6dd6d8891
asoral <asoral@dexciss.com>
2021-09-28 10:02:38 +0530

    Purchase VAT and Sales VAT report

---
+++ erpnext/regional/report/purchase_vat_report/purchase_vat_report.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Purchase VAT Report"] = {
+	"filters": [
+		// {
+		// 	fieldname: "year",
+		// 	label: __("Year"),
+		// 	fieldtype: "Data",
+		// 	default: "",
+		// 	reqd: 0
+		// },
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -12),
+			reqd: 1
+		},
+		{
+			fieldname:"to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options:"Company",
+			reqd: 0
+		},
+	]
+};
