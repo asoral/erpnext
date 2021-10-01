2b258d70439e166086f661db2a52e211a06745c6
Ajay Jogdand <ajayjogdand374@gmail.com>
2021-10-01 15:28:33 +0530

    chnages

---
+++ erpnext/selling/doctype/cost_calculator/cost_calculator_dashboard.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'cost_calculator',
+		'transactions': [
+			{
+				'label': _('Quuotation'),
+				'items': ['Quotation']
+			},
+			{
+				'label': _('Item'),
+				'items': ['Item']
+			}
+		]
+	}
