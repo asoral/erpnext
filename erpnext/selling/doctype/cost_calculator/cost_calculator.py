ac7af00e864faa043de45043b232584792da095e
Ajay Jogdand <ajayjogdand374@gmail.com>
2021-12-28 11:39:09 +0530

    Changes

--- erpnext/selling/doctype/cost_calculator/cost_calculator.py
+++ erpnext/selling/doctype/cost_calculator/cost_calculator.py
@@ -728,8 +728,9 @@ class CostCalculator(Document):
 							c=eval(d)
 							for i in c:
 								formula=formula.replace(i,str(c[i]))
+							print("##############3",formula)
 							formu=eval(formula)
-							print(formu)
+							print("&&&&&&&&&&&&&&&",k.attribute,formu)
 							a+="'"+str(k.attribute)+"'"+":"+"'"+str(formu)+"'"+","+"\n"
 					except:
 						print("")
@@ -776,7 +777,9 @@ class CostCalculator(Document):
 						for i in c:
 							formula=formula.replace(i,str(c[i]))
 						formu=eval(formula)
+						print("##############3",formula)
 						j.wp_unit=formu
+						print("&&&&&&&&&&&&&&&",i.idx,formu)
 				except:
 					print("")
 			self.get_qty()
