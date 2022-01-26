5a25611c3c6eecd82f52508bf39fc41e32d2873c
Ajay Jogdand <ajayjogdand374@gmail.com>
2022-01-26 11:30:51 +0530

    chnges

--- erpnext/payroll/doctype/salary_slip/salary_slip.js
+++ erpnext/payroll/doctype/salary_slip/salary_slip.js
@@ -157,18 +157,7 @@ frappe.ui.form.on("Salary Slip", {
 	},
 
 	refresh: function(frm) {
-		if(frm.doc.__islocal){
-			frappe.call({
-				method: 'get_payroll',
-				doc:frm.doc,
-				callback: function(r) {
-					if(r.message) {
-					frm.set_value('months_of_service_in_payment_period', r.message);
-					frm.refresh_field("months_of_service_in_payment_period");
-					}
-				}
-			});
-		}
+		
 		frm.trigger("toggle_fields");
 		
 		
@@ -244,6 +233,17 @@ frappe.ui.form.on("Salary Slip", {
 	},
 
 	toggle_fields: function(frm) {
+		frappe.call({
+			method: 'get_payroll',
+			doc:frm.doc,
+			callback: function(r) {
+				if(r.message) {
+				// frm.set_value('months_of_service_in_payment_period', r.message);
+				frm.doc.months_of_service_in_payment_period=r.message
+				frm.refresh_field("months_of_service_in_payment_period");
+				}
+			}
+		});
 		frm.toggle_display(['hourly_wages', 'timesheets'], cint(frm.doc.salary_slip_based_on_timesheet)===1);
 
 		frm.toggle_display(['payment_days', 'total_working_days', 'leave_without_pay'],
