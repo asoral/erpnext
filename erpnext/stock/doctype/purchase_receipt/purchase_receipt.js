1c60dc37384b31e9522d2dddae1e5ced79b2d487
asoral <soral.apoorv@gmail.com>
2022-07-26 17:15:33 +0530

    Update purchase_receipt.js

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.js
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.js
@@ -302,32 +302,32 @@ erpnext.stock.PurchaseReceiptController = erpnext.buying.BuyingController.extend
 		}
 
 		if(!this.frm.doc.is_return && this.frm.doc.status!="Closed") {
-			if (this.frm.doc.docstatus == 0) {
-				this.frm.add_custom_button(__('Purchase Order'),
-					function () {
-						if (!me.frm.doc.supplier) {
-							frappe.throw({
-								title: __("Mandatory"),
-								message: __("Please Select a Supplier")
-							});
-						}
-						erpnext.utils.map_current_doc({
-							method: "erpnext.buying.doctype.purchase_order.purchase_order.make_purchase_receipt",
-							source_doctype: "Purchase Order",
-							target: me.frm,
-							setters: {
-								supplier: me.frm.doc.supplier,
-								schedule_date: undefined
-							},
-							get_query_filters: {
-								docstatus: 1,
-								status: ["not in", ["Closed", "On Hold"]],
-								per_received: ["<", 99.99],
-								company: me.frm.doc.company
-							}
-						})
-					}, __("Get Items From"));
-			}
+// 			if (this.frm.doc.docstatus == 0) {
+// 				this.frm.add_custom_button(__('Purchase Order'),
+// 					function () {
+// 						if (!me.frm.doc.supplier) {
+// 							frappe.throw({
+// 								title: __("Mandatory"),
+// 								message: __("Please Select a Supplier")
+// 							});
+// 						}
+// 						erpnext.utils.map_current_doc({
+// 							method: "erpnext.buying.doctype.purchase_order.purchase_order.make_purchase_receipt",
+// 							source_doctype: "Purchase Order",
+// 							target: me.frm,
+// 							setters: {
+// 								supplier: me.frm.doc.supplier,
+// 								schedule_date: undefined
+// 							},
+// 							get_query_filters: {
+// 								docstatus: 1,
+// 								status: ["not in", ["Closed", "On Hold"]],
+// 								per_received: ["<", 99.99],
+// 								company: me.frm.doc.company
+// 							}
+// 						})
+// 					}, __("Get Items From"));
+// 			}
 
 			if(this.frm.doc.docstatus == 1 && this.frm.doc.status!="Closed") {
 				if (this.frm.has_perm("submit")) {
