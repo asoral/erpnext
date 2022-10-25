83bb1699e1066bae980f9000c798d85587782519
nirav <asoral@dexciss.com>
2022-10-25 16:06:34 +0400

    Fix:Removed Nepali Date Doctype From version-13-grandhyper branch

--- erpnext/accounts/doctype/gl_entry/gl_entry.py
+++ erpnext/accounts/doctype/gl_entry/gl_entry.py
@@ -38,9 +38,7 @@ class GLEntry(Document):
 			self.check_pl_account()
 			self.validate_party()
 			self.validate_currency()
-		company = frappe.db.get_single_value("System Settings",'country')
-		if company == 'Nepal':
-			self.set_nepali_date()
+		
 
 	def on_update(self):
 		adv_adj = self.flags.adv_adj
@@ -87,11 +85,7 @@ class GLEntry(Document):
 					self.voucher_type)
 
 				frappe.throw(msg, title=_("Missing Cost Center"))
-	def set_nepali_date(self):
-		from erpnext.nepali_date import get_converted_date
-		if self.posting_date:
-			nepali_date = get_converted_date(self.posting_date)
-			self.posting_datenepali = nepali_date
+	
 	def validate_dimensions_for_pl_and_bs(self):
 		account_type = frappe.db.get_value("Account", self.account, "report_type")
 
